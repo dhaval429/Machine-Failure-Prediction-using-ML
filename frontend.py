283e414ed25e7972b38fc8aa1ac30958b3182e66
dhaval429 <80951015+dhaval429@users.noreply.github.com>
2023-04-17 02:15:47 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -18,7 +18,7 @@ Input4 = st.slider("Enter the value of wear and tear (Standardised value b/w 0 a
 Input5 = st.number_input("Enter reliability index")
 Input6 = st.number_input("Enter the value of usage frequency")
 Input7 = st.number_input("Enter the operating temperature")
-Input8 = st.selectbox("Enter the operating knowledge", (1,2,3))
+Input8 = st.number_input("Enter the operating knowledge")
 
 lst = [[Input1, Input2, Input3, Input4, Input5, Input6, Input7, Input8]]
 X_test = pd.DataFrame(lst, columns=['AGE_OF_EQUIPMENT','TIME_SINCE_MAINTENANCE','CORROSION','WEAR_AND_TEAR','RELIABILITY','USAGE_FREQUENCY','OPERATING_TEMPARATURE','OPERATING_KNOWLEDGE'])
