5e951295df9bdcf0600e0bd4b44b5d97a4d73af6
dhaval429 <80951015+dhaval429@users.noreply.github.com>
2023-04-17 01:59:52 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -7,25 +7,30 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-st.title("BTP")
 
-st.write("Machine Failure Prediction using ML")
 
-Input1 = st.number_input("Enter the age of equipment")
-Input2 = st.number_input("Enter time since maintenance")
-Input3 = st.number_input("Enter the value of corrosion")
-Input4 = st.number_input("Enter the value of wear and tear")
+st.title("Machine Failure Prediction using ML")
+
+Input1 = st.number_input("Enter the age of equipment (in days)")
+Input2 = st.number_input("Enter time since maintenance (in days)")
+Input3 = 0
+Input4 = st.slider("Enter the value of wear and tear (Standardised value b/w 0 and 1)", 0.0, 1.0)
 Input5 = st.number_input("Enter reliability index")
 Input6 = st.number_input("Enter the value of usage frequency")
-Input7 = st.number_input("Enter the operating frequency")
-Input8 = st.number_input("Enter the operating knowledge")
+Input7 = st.number_input("Enter the operating temperature")
+Input8 = st.selectbox("Enter the operating knowledge", (1,2,3))
 
 lst = [[Input1, Input2, Input3, Input4, Input5, Input6, Input7, Input8]]
 X_test = pd.DataFrame(lst, columns=['AGE_OF_EQUIPMENT','TIME_SINCE_MAINTENANCE','CORROSION','WEAR_AND_TEAR','RELIABILITY','USAGE_FREQUENCY','OPERATING_TEMPARATURE','OPERATING_KNOWLEDGE'])
 st.write(X_test)
 loaded_model = pickle.load(open("model.h5", 'rb'))
-result = loaded_model.predict(X_test)
 
-st.write(result)
+if st.button("Find Result!"):
+    result = loaded_model.predict(X_test)
+
+    if result==0:
+        st.subheader('The machine will not fail.')
+    else:
+        st.subheader('The machine requires maintenance.')
 
 
